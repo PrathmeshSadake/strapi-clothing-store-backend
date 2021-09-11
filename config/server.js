41b62f45e89e80c5a780f19b083890fc09040e52
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-09-11 20:35:57 +0530

    created basic api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '52a7b354d5019810614a5207fd44221e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e43ba97f-68b1-492a-8e58-ef020a09d2aa'
+};
